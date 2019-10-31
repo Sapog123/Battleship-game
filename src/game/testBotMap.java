c8c60c4c64e18e4a8eb6e72bd002aec7ddc9e190
Sapog123 <verasapozhnickova@yandex.ru>
2019-10-31 17:09:27 +0500

    тесты

---
+++ src/game/testBotMap.java
@@ -0,0 +1,54 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testBotMap {
+
+	int CountDamagedCell(BotMap botMap)
+	{
+		int curDamagedCell = 0;
+		for (int i = 0; i < botMap.size; i++)
+		{
+			for (int j = 0; j < botMap.size; j++)
+			{
+				if (botMap.GetStateCell(i, j) == Report.damage)
+					curDamagedCell++;
+			}
+		}
+		return curDamagedCell;
+	}
+	@Test
+	void testBotMapInit() {
+		int damagedCell = 20;
+		BotMap botMap= new BotMap();
+		int curDamagedCell = this.CountDamagedCell(botMap);
+		assertEquals(10, botMap.size);
+		assertEquals(10, botMap.countShipsAlive());
+		assertEquals(damagedCell, curDamagedCell);
+	}
+	
+	@Test
+	void testBotMapEnd() {
+		int killedShip = 10;
+		int curKilledShip = 0;
+		BotMap botMap = new BotMap();
+		for (int i = 0; i < botMap.size; i++)
+		{
+			for (int j = 0; j < botMap.size; j++)
+			{
+				if (botMap.GetStateCell(i, j) == Report.damage)
+				{
+					if (botMap.ChangeState(i, j) == Report.kill)
+						curKilledShip++;
+				}
+			}
+		}
+		int curDamagedCell = this.CountDamagedCell(botMap);
+		assertEquals(0, botMap.countShipsAlive());
+		assertEquals(killedShip, curKilledShip);
+		assertEquals(0, curDamagedCell);
+	}
+}
+
---
+++ src/game/testBotShip.java
@@ -0,0 +1,60 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testBotShip {
+
+	int sizeMap = 10;
+	int countDeck = 4;
+	int position = 20;
+	Orientation orientation = Orientation.horizontally;
+	int number = 1;
+	int x = position % sizeMap;
+	int y = position / sizeMap;
+
+	@Test
+	void testBotShipInit() {
+		BotShip ship = new BotShip(countDeck, position, orientation, number, x, y);
+		assertEquals(State.alive, ship.State());
+		assertEquals(countDeck, ship.CountDeck());
+		assertEquals(position, ship.Position());
+		assertEquals(orientation, ship.Orientation());
+		assertEquals(number, ship.IdNumber);
+		assertEquals(x, ship.X);
+		assertEquals(y, ship.Y);
+		assertEquals(countDeck, ship.ScoreAlive());
+	}
+	
+	@Test
+	void testBotShipChangeStateDamaged() {
+		BotShip ship = new BotShip(countDeck, position, orientation, number, x, y);
+		ship.changState();
+		assertEquals(State.damaged, ship.State());
+		assertEquals(countDeck - 1, ship.ScoreAlive());
+		assertEquals(countDeck, ship.CountDeck());
+		assertEquals(position, ship.Position());
+		assertEquals(orientation, ship.Orientation());
+		assertEquals(number, ship.IdNumber);
+		assertEquals(x, ship.X);
+		assertEquals(y, ship.Y);
+	}
+	
+	@Test
+	void testBotShipChangeStateKilled() {
+		BotShip ship = new BotShip(countDeck, position, orientation, number, x, y);
+		for (int i = 0; i < countDeck; i++)
+			ship.changState();
+		assertEquals(State.killed, ship.State());
+		assertEquals(0, ship.ScoreAlive());
+		assertEquals(countDeck, ship.CountDeck());
+		assertEquals(position, ship.Position());
+		assertEquals(orientation, ship.Orientation());
+		assertEquals(number, ship.IdNumber);
+		assertEquals(x, ship.X);
+		assertEquals(y, ship.Y);
+	}
+
+}
+
